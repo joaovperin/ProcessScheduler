acffb0548fc4a9d1a5faf47a77e316f7bf5f7660
Perin <perin@rech.com.br>
2018-11-22 12:42:40 -0200

    Created Hello World Project

---
+++ src/br/feevale/jpe/Main.java
@@ -0,0 +1,20 @@
+/*
+ * ProcessScheduler
+ * CopyRight Rech Informática Ltda. Todos os direitos reservados.
+ */
+package br.feevale.jpe;
+
+/**
+ *
+ * @author joaovperin
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm a process scheduler :D");
+    }
+
+}
